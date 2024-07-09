95562922f3da8e883f504d5d3efd90f562a34f06
Ticca-Liyin <107807178+Ticca-Liyin@users.noreply.github.com>
2024-07-09 14:42:20 +0800

    v2.3.5

--- src/stores/setting.js
+++ src/stores/setting.js
@@ -1,10 +1,10 @@
 import {ref, watchEffect, computed } from 'vue'
 import { defineStore } from 'pinia'
 
-export const useSettingStore = defineStore('setting', () => {
+export const useSettingStore = defineStore('setting', () => { 
     const ACHIEVEMENT_SELECT_ALL_SECOND_CONFIRMATION_KEY = 'achievement-select-all-second-confirmation'
 
-    const achievementSelectAllSecondConfirmation = ref(JSON.parse(localStorage.getItem(ACHIEVEMENT_SELECT_ALL_SECOND_CONFIRMATION_KEY) ?? false))
+    const achievementSelectAllSecondConfirmation = ref(JSON.parse(localStorage.getItem(ACHIEVEMENT_SELECT_ALL_SECOND_CONFIRMATION_KEY) ?? true))
 
     const secondConfirmationList = [
         {
