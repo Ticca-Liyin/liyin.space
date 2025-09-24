24a139029a55fd22b26f43dea0cb16cc908e3c86
LiYin <2264492866@qq.com>
2025-09-24 22:59:46 +0800

    v3.6.0a

--- src/utils/version.js
+++ src/utils/version.js
@@ -2,7 +2,7 @@ export const achievementInfoVersion = '3.6.0'
 
 export const achievementSeriesVersion = '3.4.3'
 
-export const multipleChoiceVersion = '3.6.0'
+export const multipleChoiceVersion = '3.6.1'
 
 export const notAvailableAchievementVersion = '3.4.3'
 
